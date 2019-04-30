7474a8326b2c9cecff1d8f9f844211eaa1912474
Alex <alexandrareginawilson@gmail.com>
2019-04-30 16:03:41 -0500

    First commit using 07-Ins_Mern as a template

---
+++ client/build/precache-manifest.410334193c2ff752caa49bc303e10491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "413d4a92387b7436f166",
+    "url": "/static/js/main.3489fff3.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "413d4a92387b7436f166",
+    "url": "/static/css/main.f4ccc882.chunk.css"
+  },
+  {
+    "revision": "79accb7f847af944e3e88e344a71611e",
+    "url": "/index.html"
+  }
+];
